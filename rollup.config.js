788e364910f2416770f7dd92547c99a54ee617d3
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2026-05-04 11:11:17 +0900

    setup packaging

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+// See: https://rollupjs.org/introduction/
+
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json';
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+
+const config = {
+  input: 'src/main.js',
+  output: {
+    esModule: true,
+    file: 'dist/index.js',
+    format: 'es',
+    sourcemap: true
+  },
+  plugins: [commonjs(), json(), nodeResolve({ preferBuiltins: true })]
+}
+
+export default config
